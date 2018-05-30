0094e6bd392d24d45178c1dafc3f576661d22f1e
youssef324234 <35976372+youssef9898@users.noreply.github.com>
2018-05-30 13:41:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const http2 = require('http2');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '1';
-const discord_token = "NDM3OTUyNjA4MzIyMzIyNDMy.De84xw.dA0RXdM4WGvbDQxcB1gxHQHDZu0";
+const discord_token = "NDUxMzM0MTY5NzY3NzA2NjI2.DfARNw.Zw0TtgdIy2uDdd1TqjCDrC7W4Yc";
 client.login(discord_token);
 client.on('ready', function() {
  console.log(`Logged in as * [ " ${client.user.username} " ]`);
