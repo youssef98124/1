c0c09255334ddfb2bfac00ce188d48b98f0091a2
youssef324234 <35976372+youssef9898@users.noreply.github.com>
2018-06-03 02:30:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إخالمقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 
-					.setFooter("CODES")
+					.setFooter("best")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
