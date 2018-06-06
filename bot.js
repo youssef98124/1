89ef7c13c65e445a7142d095a22ee5cda5ba0cec
BOTQWEREW123 <39924208+BOTQWEREW123@users.noreply.github.com>
2018-06-07 02:52:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 var Canvas = require('canvas');
 var jimp = require('jimp');
 const fs = require("fs");
 
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 client.on('message', message => {
