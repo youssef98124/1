c73c61875bc098d47745a2ca00f4afbdb9826562
BOTQWEREW123 <39924208+BOTQWEREW123@users.noreply.github.com>
2018-06-07 04:23:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,10 @@ const client = new Discord.Client();
 const fs = require("fs");
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`رمضان كريم`,'https://www.twitch.tv/v5bz');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+
 
 client.on('message', message => {
   var prefix = '=';
