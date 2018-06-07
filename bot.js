adf202b0cc594e92175271a0ea6584c1eb6c71ea
BOTQWEREW123 <39924208+BOTQWEREW123@users.noreply.github.com>
2018-06-07 04:20:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,7 +138,18 @@ client.on('message', message => {
 
 
 
-
+client.on('message', message => {
+    if(message.content === "=bot") {
+        const embed = new Discord.RichEmbed()
+        .setColor("#00FFFF")
+  .addField('**الذاكرة المستخدمة 💾**', `${(process.memoryUsage().rss / 1000000).toFixed()}MB`, true)
+         .addField('**سرعة الاتصال📡**' , `${Date.now() - message.createdTimestamp}` + ' ms')
+        .addField('**استخدام المعالج💿**', `${(process.cpuUsage().rss / 10000).toFixed()}%`, true)
+        .addField('**:globe_with_meridians: عدد السيرفرات**' , `${client.guilds.size}`, true)
+        .addField('**عدد المستخدمين 👥 **' , `${client.users.size}`, true)
+               message.channel.sendEmbed(embed);
+           }
+});
 
 
 
