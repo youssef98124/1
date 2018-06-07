c090648ce5da3d7b889564ff720c8932678c2644
BOTQWEREW123 <39924208+BOTQWEREW123@users.noreply.github.com>
2018-06-08 01:26:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
 const moment = require('moment');
+var Canvas = require('canvas');// npm i canvas
+var jimp = require('jimp');// npm i jimp 
+const fs = require("fs");// npm i fs
 
 client.on('ready', () => {
      client.user.setActivity("Y-Bot",{type: 'WATCHING'});
